e25ce0b3ddbb2c943b6d753e8a4d407a71292b7a
Christian Pojoni <christian@Christians-MacBook-Pro.local>
2020-10-05 18:37:05 +0200

    models and migrations

---
+++ database/migrations/2020_10_05_162906_create_menu_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenuTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->integer('kcal');
+            $table->integer('bread_units');
+            $table->string('allergens');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu');
+    }
+}
